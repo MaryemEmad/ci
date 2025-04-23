e7f899e4dfa1402abad8d9546dd184d9f0b8862a
MaryemEmad <104657581+MaryemEmad@users.noreply.github.com>
2025-04-23 23:16:08 +0200

    Update ga_clustering.py

--- ga_clustering.py
+++ ga_clustering.py
@@ -1,1 +1,38 @@
+# ga_clustering.py: Implements Genetic Algorithm for clustering
+import numpy as np
 
+class GAClustering:
+    def __init__(self, n_clusters):
+        self.n_clusters = n_clusters
+        self.population_size = 50
+        self.generations = 100
+        self.mutation_rate = 0.1  # Parameter to vary (guideline f)
+
+    def initialize_population(self, data_size):
+        # Representation: Each individual is a list of cluster assignments
+        return np.random.randint(0, self.n_clusters, size=(self.population_size, data_size))
+
+    def compute_fitness(self, data, labels):
+        # Fitness: Within-cluster sum of squares (to minimize)
+        centroids = np.array([np.mean(data[labels == k], axis=0) for k in range(self.n_clusters)])
+        sse = sum(np.sum((data[labels == k] - centroids[k])**2) for k in range(self.n_clusters))
+        return sse
+
+    def fit(self, data):
+        # Placeholder for GA implementation (inspired by Paper 1)
+        population = self.initialize_population(len(data))
+        best_solution = population[0]
+        best_fitness = float('inf')
+
+        for _ in range(self.generations):
+            # Evaluate fitness
+            fitness_values = [self.compute_fitness(data, ind) for ind in population]
+            best_idx = np.argmin(fitness_values)
+            if fitness_values[best_idx] < best_fitness:
+                best_fitness = fitness_values[best_idx]
+                best_solution = population[best_idx].copy()
+
+            # Selection, crossover, mutation (to be implemented)
+            population = np.random.randint(0, self.n_clusters, size=(self.population_size, len(data)))
+
+        return best_solution
