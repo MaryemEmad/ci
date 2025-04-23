077a80d88304569ce5c9708f6a8456ae0f900fd5
MaryemEmad <104657581+MaryemEmad@users.noreply.github.com>
2025-04-23 23:18:25 +0200

    Update fa_clustering.py

--- fa_clustering.py
+++ fa_clustering.py
@@ -1,1 +1,37 @@
+# fa_clustering.py: Implements Firefly Algorithm for clustering
+import numpy as np
 
+class FAClustering:
+    def __init__(self, n_clusters):
+        self.n_clusters = n_clusters
+        self.n_fireflies = 20
+        self.iterations = 100
+        self.beta = 1.0  # Parameter to vary (guideline f)
+
+    def initialize_fireflies(self, data_size):
+        # Representation: Each firefly is a list of cluster assignments
+        return np.random.randint(0, self.n_clusters, size=(self.n_fireflies, data_size))
+
+    def compute_fitness(self, data, labels):
+        # Fitness: Within-cluster sum of squares (to minimize)
+        centroids = np.array([np.mean(data[labels == k], axis=0) for k in range(self.n_clusters)])
+        sse = sum(np.sum((data[labels == k] - centroids[k])**2) for k in range(self.n_clusters))
+        return sse
+
+    def fit(self, data):
+        # Placeholder for FA implementation (inspired by Paper 4)
+        fireflies = self.initialize_fireflies(len(data))
+        best_solution = fireflies[0]
+        best_fitness = float('inf')
+
+        for _ in range(self.iterations):
+            # Firefly attraction and movement (to be implemented)
+            fitness_values = [self.compute_fitness(data, ff) for ff in fireflies]
+            best_idx = np.argmin(fitness_values)
+            if fitness_values[best_idx] < best_fitness:
+                best_fitness = fitness_values[best_idx]
+                best_solution = fireflies[best_idx].copy()
+
+            fireflies = np.random.randint(0, self.n_clusters, size=(self.n_fireflies, len(data)))
+
+        return best_solution
