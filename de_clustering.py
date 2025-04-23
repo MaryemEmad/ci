2d1ca040f7e99972bf672ea8c4de7b9c9c39e00b
MaryemEmad <104657581+MaryemEmad@users.noreply.github.com>
2025-04-23 23:18:44 +0200

    Update de_clustering.py

--- de_clustering.py
+++ de_clustering.py
@@ -1,1 +1,38 @@
 
+# de_clustering.py: Implements Differential Evolution for clustering
+import numpy as np
+
+class DEClustering:
+    def __init__(self, n_clusters):
+        self.n_clusters = n_clusters
+        self.population_size = 30
+        self.iterations = 100
+        self.F = 0.8  # Parameter to vary (guideline f)
+
+    def initialize_population(self, data_size):
+        # Representation: Each individual is a list of cluster assignments
+        return np.random.randint(0, self.n_clusters, size=(self.population_size, data_size))
+
+    def compute_fitness(self, data, labels):
+        # Fitness: Within-cluster sum of squares (to minimize)
+        centroids = np.array([np.mean(data[labels == k], axis=0) for k in range(self.n_clusters)])
+        sse = sum(np.sum((data[labels == k] - centroids[k])**2) for k in range(self.n_clusters))
+        return sse
+
+    def fit(self, data):
+        # Placeholder for DE implementation (inspired by Paper 5)
+        population = self.initialize_population(len(data))
+        best_solution = population[0]
+        best_fitness = float('inf')
+
+        for _ in range(self.iterations):
+            # Differential evolution (mutation, crossover, selection) (to be implemented)
+            fitness_values = [self.compute_fitness(data, ind) for ind in population]
+            best_idx = np.argmin(fitness_values)
+            if fitness_values[best_idx] < best_fitness:
+                best_fitness = fitness_values[best_idx]
+                best_solution = population[best_idx].copy()
+
+            population = np.random.randint(0, self.n_clusters, size=(self.population_size, len(data)))
+
+        return best_solution
